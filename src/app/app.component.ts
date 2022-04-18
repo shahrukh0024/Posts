5a9184c0ab684ee9e6d0391b612a11bf5c9b2290
Shahrukh Pathan <s.pathan@zensar.com>
2022-04-18 14:15:28 +0530

    Form task completed and showed list of users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 type TUser = {
-  name : string;
-  age : number;
-  bio : string;
-  gender : string;
+  name: string;
+  age: number;
+  bio: string;
+  gender: string;
 }
 
 
@@ -13,31 +13,28 @@ type TUser = {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
-  name : string; 
-  age : number;
-  bio : string;
-  gender : string;
-  // name="";
-  // age ="";
-  // bio ="";
-  // gender ="";
-  user : TUser[] = []
-  
-  handleUser(event : MouseEvent)
-  {
+export class AppComponent {
+  name: string;
+  age: number;
+  bio: string;
+  gender: string;
+
+  users: TUser[] = []
+
+  handleUser(event: MouseEvent) {
     event.preventDefault();
-    
-    console.log(this.name,this.age,this.bio,this.gender)
-    this.user.push(
-      {name : this.name,
-      age : this.age,
-      bio : this.bio,
-      gender : this.gender
+
+    console.log(this.name, this.age, this.bio, this.gender)
+    this.users.unshift(
+      {
+        name: this.name,
+        age: this.age,
+        bio: this.bio,
+        gender: this.gender
       }
     )
-    console.log(this.user)
-    
-  
+    console.log(this.users)
+
+
   }
 }
